e6d17f1615c312161635e370e954f254424f4941
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2025-04-26 23:23:18 +0500

    kookie qoshildi gmail sms keladigon qilindi

--- src/utils/admin.validation.js
+++ src/utils/admin.validation.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 const admin = Joi.object({
-    username: Joi.string().min(4).max(20).required(),
-    password: Joi.string().min(8).max(22).required()
+  username: Joi.string().min(4).max(20).required(),
+  password: Joi.string().min(8).max(22).required(),
 });
 
 export const adminValidator = (data) => {
-    return admin.validate(data);
-}
+  return admin.validate(data);
+};
